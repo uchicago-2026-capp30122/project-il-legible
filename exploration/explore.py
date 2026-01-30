f52eecbc3cc73ac74975cb1518cee8e5f583f1cf
Brock Sauvage <bsauvage14@gmail.com>
2026-01-30 10:46:09 -0600

    Restoring bulk data pull code

--- exploration/explore.py
+++ exploration/explore.py
@@ -5,83 +5,31 @@ import time
 from dotenv import load_dotenv
 from pathlib import Path
 import pandas as pd
-import exploration.endpoint_params as params
-
-def get_api_ref():
-    try:
-        filepath = Path(__file__).parent / "api_ref.json"
-        with open(filepath, "r") as file:
-            api_ref = json.load(file)
-        return api_ref
-    except FileNotFoundError:
-        print("Error Opening API Reference File: Not Found")
-  
-API_KEY = os.getenv("OPEN_STATES_API_KEY")
-API_REF = get_api_ref()
 
 def main():
     print("Hello from project-il-legible!")
     bills = get_bills()
-    people = get_people()
 
     print(bills)
 
     
-def construct_endpoint_url(endpoint, id=None, params=None):
-    url = httpx.URL(f"{API_REF["base_url"]}{endpoint}/{id if id is not None else ''}?apikey={API_KEY}")
 
-    if(params):
-        params = dict(url.params) | params
-        url = url.copy_with(params=params)
+def get_bills():
+    return get_data_from_csv("bills")
 
-    return str(url)
+# def get_people(person_id=None, max_pages=1):
+#     return get_data_from_csv(url)
+
+def get_sponsors():
+    return get_data_from_csv("sponsorships")
+
+def get_data_from_csv(file_fragment):
+    root_dir = Path(os.getcwd())
+    file_list = list(root_dir.rglob(f"./*/bulk_data/IL/*/*{file_fragment}.csv")) 
+    df = None
+    df = pd.concat((pd.read_csv(f) for f in file_list), ignore_index=True)
     
-
-def get_bills(max_pages=100):
-    
-    bills = []
-    page = 1
-
-    for i in range(1, max_pages+1):
-        q_params = params.BILLS | {"page": i}
-        url = construct_endpoint_url("/bills", params=(params.BILLS | {"page": page}))
-        data = make_request(url)
-        bills.extend(data["results"])
-
-        pg = data["pagination"]
-
-        if(pg["page"] == max_pages or pg["page"] == pg["max_page"]):
-            break
-
-        page = pg["page"] + 1
-        
-    return bills
-
-def get_people(person_id=None, max_pages=1):
-    url = construct_endpoint_url("/people", id=person_id, params=params.PEOPLE)
-    return make_request(url)
-
-
-
-def get_params_keys(endpoint):
-    """
-    Get a list of param keys for a given endpoint
-    
-    :param endpoint: The endpoint for which to get params
-    """
-    params = API_REF["endpoints"][endpoint]["search"]["query_parameters"]
-    return params.keys()
-
-
-
-def make_request(url):
-    try:
-        time.sleep(6)
-        resp = httpx.get(url, follow_redirects=True)
-        return resp.json()
-    except:
-        print("Error making request to ", url)
-
+    return df
 
 
 if __name__ == "__main__":
