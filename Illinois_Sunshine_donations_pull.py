eddb777ec3b84c87e459f6b4ba214bbb457dc98b
enowlis <enowlis@uchicago.edu>
2026-02-03 12:26:19 -0600

    Framework for sponsor/commitee matching

--- Illinois_Sunshine_donations_pull.py
+++ Illinois_Sunshine_donations_pull.py
@@ -7,6 +7,13 @@ import csv
 import httpx
 import io
 
+def match_sponsor_to_committee(name: str):
+    ## First step: use the lookup URL, use the JSON file it returns to match
+    ## names and get relevant ID candidate numbers [Elie]
+
+    ## Second step: go to candidate pages using ID numbers [Max]
+    ## (https://illinoissunshine.org/candidates/7821/) and pull committee ID numbers
+
 def download_donations(committees: list):
     """
     [[write a doc string]]
