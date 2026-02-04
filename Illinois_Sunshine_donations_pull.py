0facd2d290e9c44ae963100c8af236a4caaa2d3e
enowlis <enowlis@uchicago.edu>
2026-02-03 22:44:52 -0600

    All functions working

--- Illinois_Sunshine_donations_pull.py
+++ Illinois_Sunshine_donations_pull.py
@@ -7,14 +7,57 @@ import csv
 import httpx
 import io
 import lxml.html
-import re
+# import re
+import json
 # import lxml.cssselect
 
-def match_sponsor_to_committee(name: str):
-    ## First step: use the lookup URL, use the JSON file it returns to match
-    ## names and get relevant ID candidate numbers [Elie]
-    return
+def match_sponsor_to_candidate(name: str) -> list[str]:
+    """
+    First step: use the lookup URL, use the JSON file it returns to match
+    names and get relevant ID candidate numbers [Elie]
+    [[make this better]]
+    """
+    first_name = name.split()[0]
+    last_name = name.split()[1]
 
+    url = "https://illinoissunshine.org/api/advanced-search/"
+
+    headers = {
+        "referer" : "https://illinoissunshine.org/search/?term=Sara+Feigenholtz&table_name=candidates&search_date__ge=&search_date__le="
+    }
+
+    params = {
+        "term" : name,
+        "table_name" : "candidates",
+        "draw" : "1",
+        "columns[0][data]" : "last_name",
+        "columns[0][searchable]" : "true",
+        "columns[0][orderable]" : "true",
+        "columns[0][search][regex]" : "false",
+        "columns[1][data]" : "party",
+        "columns[1][searchable]" : "true",
+        "columns[1][orderable]" : "true",
+        "columns[1][search][regex]" : "false",
+        "columns[2][data]" : "office",
+        "columns[2][searchable]" : "true",
+        "columns[2][orderable]" : "true",
+        "columns[2][search][regex]" : "false",
+        "order[0][column]" : "0",
+        "order[0][dir]" : "asc",
+        "start" : "0",
+        "length" : "10",
+        "search[regex]" : "false",
+        "_" : "1770178172833"
+    }
+
+    id_list = []
+    with httpx.Client(headers = headers) as c:
+        response = c.get(url, params = params)
+        json_dic = json.loads(response.text)
+        for candidate in json_dic["objects"]["candidates"]:
+            if candidate["first_name"] == first_name and candidate["last_name"] == last_name:
+                id_list.append(candidate["id"])
+        return id_list
 
 
 def get_committee_ids(candidate_ids: list[str]):
@@ -24,7 +67,7 @@ def get_committee_ids(candidate_ids: list[str]):
     unique_committee_ids = set()
     for candidate_id in candidate_ids:
         response = httpx.get(f"https://illinoissunshine.org/candidates/{candidate_id}/")
-        root = lxml.html.fromstring(response.text)
+        root = html.fromstring(response.text)
         
         table = root.cssselect("table.table.table-striped")[0]
         
@@ -37,28 +80,7 @@ def get_committee_ids(candidate_ids: list[str]):
     return committee_ids
 
 
-
-def get_committee_ids(candidate_ids: list[str]):
-    """
-    [[write a doc string]]
-    """
-    unique_committee_ids = set()
-    for candidate_id in candidate_ids:
-        response = httpx.get(f"https://illinoissunshine.org/candidates/{candidate_id}/")
-        root = lxml.html.fromstring(response.text)
-        
-        table = root.cssselect("table.table.table-striped")[0]
-        
-        for link in table.cssselect("a"):
-            committee_url = link.get("href")
-            committee_id = committee_url.rstrip("/").split("-")[-1]
-            unique_committee_ids.add(committee_id)
-            
-    committee_ids = list(unique_committee_ids)
-    return committee_ids
-
-
-def download_donations(committees: list):
+def download_donations(committees: list[str]):
     """
     [[write a doc string]]
     """
