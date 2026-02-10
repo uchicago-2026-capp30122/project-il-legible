12873fa035f2f279ac5337fdee8463b0c0102ce1
Max Manalang <manalang@uchicago.edu>
2026-02-10 11:37:25 -0600

    added more variables

--- donations_by_sponsor.py
+++ donations_by_sponsor.py
@@ -28,16 +28,24 @@ with open("donation_stats.csv", "w") as file:
     vars = [
     "Name",
     "Total_all", "Total_L3",
-    "pct_above_all", "pct_above_L3",
+    "pct_c_above_all", "pct_c_above_L3",
+    "pct_$_above_all", "pct_$_above_L3",
     "avg_donation_all", "avg_donation_L3",
     "amt_non1a_all", "amt_non1a_L3",
-    "pct_non1a_all", "pct_non1a_L3",
+    "pct_c_non1a_all", "pct_c_non1a_L3",
+    "pct_$_non1a_all", "pct_$_non1a_L3",
     "amt_nofirst_all", "amt_nofirst_L3",
-    "pct_nofirst_all", "pct_nofirst_L3",
-    "amt_LLC_all", "amt_LLC_L3",
-    "pct_LLC_all", "pct_LLC_L3",
+    "pct_c_nofirst_all", "pct_c_nofirst_L3",
+    "pct_$_nofirst_all", "pct_$_nofirst_L3",
+    "amt_entity_all", "amt_entity_L3",
+    "pct_c_entity_all", "pct_c_entity_L3",
+    "pct_$_entity_all", "pct_$_entity_L3",
+    "amt_allcond_all", "amt_allcond_L3",
+    "pct_c_allcond_all", "pct_c_allcond_L3",
+    "pct_$_allcond_all", "pct_$_allcond_L3",
     "amt_IL_all", "amt_IL_L3",
-    "pct_IL_all", "pct_IL_L3",
+    "pct_c_IL_all", "pct_c_IL_L3",
+    "pct_$_IL_all", "pct_$_IL_L3",
     "yrs_since_first", "yrs_since_last",
 ]
     writer = csv.DictWriter(file, fieldnames = vars)
@@ -71,42 +79,79 @@ with open("donation_stats.csv", "w") as file:
         # Step 2: calculate stats
         total_len = len(df)
         L3_len = df["last_3_years"].sum()
+        keywords = r"PAC|INC|LLC|Corp|Committee|Assoc|Union"
 
         row = {}
         row["Name"] = filename.replace(".csv", "")
         
-        row["Total_all"] = df["amount"].sum()
-        row["Total_L3"] = df.loc[df["last_3_years"], "amount"].sum()
+        total_all = df["amount"].sum()
+        row["Total_all"] = total_all
+        total_L3 = df.loc[df["last_3_years"], "amount"].sum()
+        row["Total_L3"] = total_L3
 
-        row["pct_above_all"] = (df["amount"] > 5000).sum() / total_len
-        row["pct_above_L3"] = (df["last_3_years"] & (df["amount"] > 5000)).sum() / L3_len
+        row["pct_c_above_all"] = (df["amount"] > 5000).sum() / total_len
+        row["pct_c_above_L3"] = (df["last_3_years"] & (df["amount"] > 5000)).sum() / L3_len
+
+        row["pct_$_above_all"] = df.loc[(df["amount"] > 5000), "amount"].sum() / total_all
+        row["pct_$_above_L3"] = df.loc[(df["last_3_years"] & (df["amount"] > 5000)), "amount"].sum() / total_L3
 
         row["avg_donation_all"] = df["amount"].mean()
         row["avg_donation_L3"] = df.loc[df["last_3_years"], "amount"].mean()
 
-        row["amt_non1a_all"] = df.loc[(df["d2_part"] != "1A"), "amount"].sum()
-        row["amt_non1a_L3"] = df.loc[((df["d2_part"] != "1A") & df["last_3_years"]) , "amount"].sum()
+        amt_non1a_all = df.loc[(df["d2_part"] != "1A"), "amount"].sum()
+        amt_non1a_L3 = df.loc[((df["d2_part"] != "1A") & df["last_3_years"]) , "amount"].sum()
+        row["amt_non1a_all"] = amt_non1a_all
+        row["amt_non1a_L3"] = amt_non1a_L3
 
-        row["pct_non1a_all"] = (df["d2_part"] != "1A").sum() / total_len
-        row["pct_non1a_L3"] = (df["last_3_years"] & (df["d2_part"] != "1A")).sum() / L3_len
+        row["pct_c_non1a_all"] = (df["d2_part"] != "1A").sum() / total_len
+        row["pct_c_non1a_L3"] = (df["last_3_years"] & (df["d2_part"] != "1A")).sum() / L3_len
 
-        row["amt_nofirst_all"] = df.loc[df["first_name"].isna(), "amount"].sum()
-        row["amt_nofirst_L3"] = df.loc[(df["first_name"].isna() & df["last_3_years"]) , "amount"].sum()
+        row["pct_$_non1a_all"] = amt_non1a_all / total_all
+        row["pct_$_non1a_L3"] = amt_non1a_L3 / total_L3
 
-        row["pct_nofirst_all"] = df["first_name"].isna().sum() / total_len
-        row["pct_nofirst_L3"] = (df["last_3_years"] & df["first_name"].isna()).sum() / L3_len
+        amt_nofirst_all = df.loc[df["first_name"].isna(), "amount"].sum()
+        amt_nofirst_L3 = df.loc[(df["first_name"].isna() & df["last_3_years"]) , "amount"].sum()
+        row["amt_nofirst_all"] = amt_nofirst_all
+        row["amt_nofirst_L3"] = amt_nofirst_L3
 
-        row["amt_LLC_all"] = df.loc[(df["last_name"].str.contains("LLC")), "amount"].sum()
-        row["amt_LLC_L3"] = df.loc[(df["last_name"].str.contains("LLC") & df["last_3_years"]) , "amount"].sum()
+        row["pct_c_nofirst_all"] = df["first_name"].isna().sum() / total_len
+        row["pct_c_nofirst_L3"] = (df["last_3_years"] & df["first_name"].isna()).sum() / L3_len
 
-        row["pct_LLC_all"] = df["last_name"].str.contains("LLC").sum() / total_len
-        row["pct_LLC_L3"] = (df["last_3_years"] & df["last_name"].str.contains("LLC")).sum() / L3_len
+        row["pct_$_nofirst_all"] = amt_nofirst_all / total_all
+        row["pct_$_nofirst_L3"] = amt_nofirst_L3 / total_L3
 
-        row["amt_IL_all"] = df.loc[(df["state"] == "IL"), "amount"].sum()
-        row["amt_IL_L3"] = df.loc[((df["state"] == "IL") & df["last_3_years"]) , "amount"].sum()
+        amt_entity_all = df.loc[df["last_name"].str.contains(keywords, case=False), "amount"].sum()
+        amt_entity_L3 = df.loc[(df["last_name"].str.contains(keywords, case=False) & df["last_3_years"]) , "amount"].sum()
+        row["amt_entity_all"] = amt_entity_all
+        row["amt_entity_L3"] = amt_entity_L3
 
-        row["pct_IL_all"] = (df["state"] == "IL").sum() / total_len
-        row["pct_IL_L3"] = (df["last_3_years"] & (df["state"] == "IL")).sum() / L3_len
+        row["pct_c_entity_all"] = df["last_name"].str.contains(keywords, case=False).sum() / total_len
+        row["pct_c_entity_L3"] = (df["last_3_years"] & df["last_name"].str.contains(keywords, case=False)).sum() / L3_len
+        
+        row["pct_$_entity_all"] = amt_entity_all / total_all
+        row["pct_$_entity_L3"] = amt_entity_L3 / total_L3
+
+        amt_allcond_all = df.loc[((df["d2_part"] != "1A") & (df["first_name"].isna()) & (df["last_name"].str.contains(keywords, case=False))), "amount"].sum()
+        amt_allcond_L3 = df.loc[((df["d2_part"] != "1A") & (df["first_name"].isna()) & (df["last_name"].str.contains(keywords, case=False)) & df["last_3_years"]) , "amount"].sum()
+        row["amt_allcond_all"] = amt_allcond_all
+        row["amt_allcond_L3"] = amt_allcond_L3
+
+        row["pct_c_allcond_all"] = ((df["d2_part"] != "1A") & (df["first_name"].isna()) & (df["last_name"].str.contains(keywords, case=False))).sum() / total_len
+        row["pct_c_allcond_L3"] = ((df["d2_part"] != "1A") & (df["first_name"].isna()) & (df["last_name"].str.contains(keywords, case=False)) & df["last_3_years"]).sum() / L3_len
+        
+        row["pct_$_allcond_all"] = amt_allcond_all / total_all
+        row["pct_$_allcond_L3"] = amt_allcond_L3 / total_L3
+
+        amt_IL_all = df.loc[(df["state"] == "IL"), "amount"].sum()
+        amt_IL_L3 = df.loc[((df["state"] == "IL") & df["last_3_years"]) , "amount"].sum()
+        row["amt_IL_all"] = amt_IL_all
+        row["amt_IL_L3"] = amt_IL_L3
+
+        row["pct_c_IL_all"] = (df["state"] == "IL").sum() / total_len
+        row["pct_c_IL_L3"] = (df["last_3_years"] & (df["state"] == "IL")).sum() / L3_len
+
+        row["pct_$_IL_all"] = amt_IL_all / total_all
+        row["pct_$_IL_L3"] = amt_IL_L3 / total_L3
 
         row["yrs_since_first"] = pd.Timestamp.today().year - pd.to_datetime(df["received_date"]).min().year
 
