747648838a0a75b0eb4d14754b733cf186c21a01
Brock Sauvage <bsauvage14@gmail.com>
2026-03-07 15:32:45 -0600

    Logging added and environment configurations set up

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap5
 from flask_scss import Scss
-
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 
 bootstrap = Bootstrap5()
@@ -42,5 +44,28 @@ def create_app():
     app.register_blueprint(insights.bp)
     app.register_blueprint(template_filters.bp)
     app.register_blueprint(api.bp)
+
+    # Logging configuration for production
+
+    if not app.debug and not app.testing:
+        # ...
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/projectillegible.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Project IL-legible startup')
 
     return app
